04b3b87de898b6e869357ba153df2df989d1c284
samzhu <spike19820318@gmail.com>
2024-12-25 11:33:44 +0800

    Add install Spring Boot & Removed specific versioning for extensions

--- workstations/200_install_extensions.sh
+++ workstations/200_install_extensions.sh
@@ -3,14 +3,14 @@ echo "Installing VSCode extensions as user 'user'..."
 
 # Extensions to install
 extensions=(
-    "vscjava.vscode-java-pack@0.29.0"
-    "VMware.vscode-boot-dev-packg@0.1.0"
-    "GabrielBB.vscode-lombok@1.0.1"
-    "redhat.fabric8-analytics@0.9.5"
-    "SonarSource.sonarlint-vscode@4.6.0"
-    "Continue.continue@0.9.239"
-    "PKief.material-icon-theme@5.1.41"
-    "mhutchie.git-graph@1.30.0"
+    "vscjava.vscode-java-pack"
+    "VMware.vscode-boot-dev-pack"
+    "GabrielBB.vscode-lombok"
+    "redhat.fabric8-analytics"
+    "SonarSource.sonarlint-vscode"
+    "Continue.continue"
+    "PKief.material-icon-theme"
+    "mhutchie.git-graph"
 )
 
 # Run installation as 'user'
